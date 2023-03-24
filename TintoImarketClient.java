3b73281f71224b646da82b305cdf316bcadd7055
Martim Paraíba <martim.paraiba@gmail.com>
2023-03-24 21:00:23 +0000

    Fixed missing import

--- TintoImarketClient.java
+++ TintoImarketClient.java
@@ -9,6 +9,7 @@
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.ConnectException;
 import java.net.Socket;
 import java.util.Arrays;
 import java.util.Scanner;
