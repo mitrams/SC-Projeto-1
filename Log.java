a9eb5c20986b16215526bc1b190de30e75a647b3
Martim Paraíba <martim.paraiba@gmail.com>
2023-04-28 16:02:46 +0100

    Simplistic implementation of Transaction logging

--- Log.java
+++ Log.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Log {
 
     private static Log self = null;
+    private long blockCount = 0;
+
+    private List<Transaction> currentTransactions = new ArrayList<Transaction>();
 
     private Log() {
     }
@@ -13,7 +19,37 @@ public class Log {
         return self;
     }
 
-    public void write(String message) {
-        System.out.println(message);
+    /*
+     * public void write(String message) {
+     * System.out.println(message);
+     * 
+     * transactionCount++;
+     * 
+     * if (transactionCount == 5) {
+     * System.out.println("New Block: " + transactionCount);
+     * if (transactionCount > 5) {
+     * System.out.println("hash_of_previous_block");
+     * } else {
+     * System.out.println(0);
+     * }
+     * System.out.println((long) blockCount);
+     * 
+     * }
+     * }
+     */
+
+    public void logTransaction(Transaction transaction) {
+        currentTransactions.add(transaction);
+
+        if (currentTransactions.size() == 5)
+            saveToFile();
+
     }
+
+    private void saveToFile() {
+        blockCount++;
+        System.out.println("Save transactions to file");
+        currentTransactions.clear();
+    }
+
 }
